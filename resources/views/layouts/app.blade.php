24782f771c376c389b3e4b399a70d9ed14156a69
Dialius <putradavinza9@gmail.com>
2025-11-03 11:22:33 +0700

    Update logo and user image in app layout for brand consistency

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -42,7 +42,7 @@
     <aside class="main-sidebar sidebar-dark-primary elevation-4">
         <!-- Brand Logo -->
         <a href="/" class="brand-link">
-            <img src="https://adminlte.io/themes/v3/dist/img/AdminLTELogo.png" alt="Logo" class="brand-image img-circle elevation-3">
+            <img src="https://i.ibb.co.com/FLmytvKN/Unlock-Your-Brand-Potential-with-Stunning-Logo-Designs.jpg" alt="Logo" class="brand-image img-circle elevation-3">
             <span class="brand-text font-weight-light">Data Pekerja</span>
         </a>
 
@@ -51,7 +51,7 @@
             <!-- Sidebar user panel -->
             <div class="user-panel mt-3 pb-3 mb-3 d-flex">
                 <div class="image">
-                    <img src="https://adminlte.io/themes/v3/dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+                    <img src="https://cdn.discordapp.com/attachments/1333890180704108554/1376042134842511421/White_angel2.jpg?ex=6909759a&is=6908241a&hm=be71316a5234842342c5070189d06b14452e28922dd8025fac3fd64336088ab7&" class="img-circle elevation-2" alt="User Image">
                 </div>
                 <div class="info">
                     <a href="#" class="d-block">Administrator</a>
